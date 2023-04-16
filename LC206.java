e6bae8dc0a5db92814915d87deb62721076efad0
antoinexu <bowei_xu@outlook.com>
2023-04-16 13:17:41 -0600

    submit lc206

---
+++ LC206.java
@@ -0,0 +1,21 @@
+public class LC206 {
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+}
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
