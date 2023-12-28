cf52a051cb8104086b938b7074c6542f6dab1d28
antoinexu <bowei_xu@outlook.com>
2023-12-28 11:35:18 -0600

    update lc83

---
+++ LC83.java
@@ -0,0 +1,16 @@
+
+public class LC83 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode cur = head;
+        while(cur != null || cur.next != null){
+            if(cur.next.val == cur.val){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
